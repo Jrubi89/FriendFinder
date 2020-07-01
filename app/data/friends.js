49a21dcfce78b5045e7c6da6ed30517b0adf7745
unknown <jrubi89@icloud.com>
2020-07-01 16:13:10 -0400

    adding array

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,1 +1,54 @@
 //array of objects//
+var friends = [
+ {
+    name: "Mat Man",
+    photo: "https://www.orange.k12.nj.us/cms/lib/NJ01000601/Centricity/Domain/548/mat-man.jpg",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+},
+{
+    name: "Elmo ",
+    photo: "https://pbs.twimg.com/profile_images/1092451830758547457/EqQ6Csl3_400x400.jpg",
+    scores: [3, 2, 6, 4, 5, 1, 2, 5, 2, 5]
+},
+{
+    name: "Big Bird",
+    photo: "https://vignette.wikia.nocookie.net/muppet/images/9/92/Bigbirdnewversion.png/revision/latest/scale-to-width-down/340?cb=20120128201030",
+    scores: [1, 5, 1, 4, 6, 2, 3, 5, 4, 3]
+},
+{
+    name: "Cookie Monster",
+    photo: "https://vignette.wikia.nocookie.net/muppet/images/0/08/CookieMonsterWaving.jpg/revision/latest?cb=20120128192952",
+    scores: [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
+},
+{
+    name: "Bert",
+    photo: "https://render.fineartamerica.com/images/rendered/default/poster/8/10/break/images/artworkimages/medium/1/bert-sesame-street.jpg",
+    scores: [5, 2, 2, 2, 4, 1, 3, 2, 5 ,6]
+},
+{
+    name: "Grover",
+    photo: "https://i.ytimg.com/vi/SKanTX1huJw/maxresdefault.jpg",
+    scores: [3, 1, 6, 4, 5, 1, 2, 5, 1, 4]
+},
+{
+    name: "Count von Count",
+    photo: "https://vignette.wikia.nocookie.net/puppet/images/5/57/Count_von_Count.jpg/revision/latest/top-crop/width/300/height/300?cb=20110923020233",
+    scores: [4, 2, 6, 4, 5, 1, 2, 6, 3, 1]
+},
+{
+    name: "Oscar the Grouch",
+    photo: "https://miro.medium.com/max/1000/0*XUqoVTLGQsYacXs_",
+    scores: [1, 3, 5, 2, 4, 1, 3, 2, 1 ,6]
+},{
+    name: "Kermit the Frog",
+    photo: "https://vignette.wikia.nocookie.net/disney/images/7/7b/Kermit_Muppets_Most_Wanted.png/revision/latest/top-crop/width/360/height/450?cb=20131130222808",
+    scores: [6, 4, 1, 4, 5, 2, 3, 1, 4, 2]
+},
+{
+    name: "Abby Cadabby",
+    photo: "https://pbs.twimg.com/profile_images/1092452467525279745/1rfX10EB_400x400.jpg",
+    scores: [5, 4, 2, 6, 4, 1, 4, 2, 5 , 3]
+}
+];
+
+module.exports = friends;
