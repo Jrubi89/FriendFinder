ebf0298aa1cbfa35f02eeb2979ab9c3b8fe26d47
unknown <jrubi89@icloud.com>
2020-07-09 13:23:43 -0400

    l

--- server.js
+++ server.js
@@ -3,18 +3,16 @@ var bodyParser = require("body-parser");
 var path = require("path");
 
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-
-app.use(express.static("app/public"));
+app.use(express.json());
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
+
 app.listen(PORT, function() {
-    console.log(" App listening on PORT " + PORT);
-});
+    console.log("App listening on PORT: " + PORT);
+  });
+  
